c658b1081e0887317a3b9f7964084f0c50ae3fb9
Julian Faraway <julian.faraway@gmail.com>
2016-10-17 10:42:17 +0100

    XF survival code

---
+++ R/tooth24.R
@@ -0,0 +1,24 @@
+#' tooth24
+#'
+#' The dataset is 
+#'
+#' @docType data
+#'
+#' @usage data(tooth24)
+#'
+#' @format The ohio data frame has 2148 rows and 4 columns
+#' \itemize{
+#' \item{resp}{an indicator of wheeze status (1=yes, 0=no).}
+#' \item{id}{a numeric vector for subject id.}
+#' \item{age}{a numeric vector of age, 0 is 9 years old}
+#' \item{smoke}{an indicator of maternal smoking at the first year of the study}
+#'}
+#' @keywords datasets
+#'
+#'
+#' @references here
+#'
+#' @examples
+#' data(tooth24)
+#'
+"tooth24"
