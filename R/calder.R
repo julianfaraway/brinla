ffcc55412e0391552c8623bd80e909908adbcdbd
julianfaraway <julian.faraway@gmail.com>
2017-01-27 14:07:33 +0000

    documentation update

---
+++ R/calder.R
@@ -0,0 +1,26 @@
+#' Maximum flows on the River Calder
+#'
+#' Maximum annual flows on the River Calder in Cumbria
+#'
+#' @docType data
+#'
+#' @usage data(calder)
+#'
+#' @format A data frame with 3 columns and 41 rows. The columns are:
+#' \itemize{
+#' \item{date}{ISO date of maximum flow that water year}
+#' \item{WaterYear}{water year which runs from Oct 1st of the previous year to Sep 30th of the current year}
+#' \item{Flow}{maximum water flow in cubic meters per second}}
+#'
+#' @details Measurement takes places at Calder Hall, grid reference NY035044
+#'
+#' @keywords datasets
+#'
+#' @source 
+#'
+#' @references Data obtained from \url{http://nrfa.ceh.ac.uk/data/station/info/74006}
+#'
+#' @examples
+#' data(calder)
+#'
+"calder"
