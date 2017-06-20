baa6d1c23144041e5a01755c5269a797bef89489
julianfaraway <julian.faraway@gmail.com>
2017-06-20 15:08:43 +0100

    wafer data added

---
+++ R/wafer.R
@@ -0,0 +1,16 @@
+#' resitivity of wafer in semiconductor experiment
+#' 
+#' A full factorial experiment with four two-level predictors.
+#' 
+#' 
+#' @name wafer
+#' @docType data
+#' @format A data frame with 16 observations on the following 5 variables.
+#' \describe{ \item{x1}{a factor with levels \code{-} \code{+}} \item{x2}{a
+#' factor with levels \code{-} \code{+}} \item{x3}{a factor with levels
+#' \code{-} \code{+}} \item{x4}{a factor with levels \code{-} \code{+}}
+#' \item{resist}{Resistivity of the wafer} }
+#' @source Myers, R. and Montgomery D. (1997) A tutorial on generalized linear
+#' models, Journal of Quality Technology, 29, 274-291.
+#' @keywords datasets
+"wafer"
