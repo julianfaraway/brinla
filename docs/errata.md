1a333c14db4ebf3bf4f3c47b475fcf531ed0410f
Julian Faraway <julian.faraway@gmail.com>
2019-07-29 10:01:10 +0100

    Ussher error

--- docs/errata.md
+++ docs/errata.md
@@ -1,5 +1,11 @@
 # Errata in Bayesian Regression with INLA
 
+- p7 The code for the fit based on the Ussher prior does not match the text. The code should read:
+```
+imod <- inla(y ~ x -1, family="gaussian", control.fixed=list(mean=uhub, prec=1/((0.05*uhub)^2)), data=hubble)
+```
+As it happens, this makes no important difference to the numerical outcome.
+
 - p104: the `inla.contrib.sd` function is no longer available in the INLA package. You can compute this information as `brinla.hyperpar.summary` function as demonstrated on the following page.
 
 - p111: 3rd line of R code should now read `mean(lvsamp[,'site:3'] > lvsamp[,'site:1'])`. At some point, the parameter labeling in INLA changed which broke the previous version of this line. 
--- docs/scripts/intro.R
+++ docs/scripts/intro.R
@@ -2,8 +2,7 @@
 # Quick Start
 
 data(hubble, package = "brinla")
-plot(y ~ x, xlab = "Distance(Mpc)", ylab = "Velocity(km/s)",
-    data = hubble)
+plot(y ~ x, xlab = "Distance(Mpc)", ylab = "Velocity(km/s)", data = hubble)
 lmod <- lm(y ~ x - 1, data = hubble)
 coef(lmod)
 hubtoage <- function(x) 3.09e+19/(x * 60^2 * 24 * 365.25 * 1e+09)
@@ -11,25 +10,24 @@ hubtoage(coef(lmod))
 (bci <- confint(lmod))
 hubtoage(bci)
 library(INLA)
-imod <- inla(y ~ x - 1, family = "gaussian",
-    control.fixed = list(prec = 1e-09), data = hubble)
+imod <- inla(y ~ x - 1, family = "gaussian", control.fixed = list(prec = 1e-09), 
+    data = hubble)
 (ibci <- imod$summary.fixed)
-plot(imod$marginals.fixed$x, type = "l", xlab = "beta",
-    ylab = "density", xlim = c(60, 100))
+plot(imod$marginals.fixed$x, type = "l", xlab = "beta", ylab = "density", 
+    xlim = c(60, 100))
 abline(v = ibci[c(3, 5)], lty = 2)
-hubtoage(ibci[c(1,3,4,5,6)])
+hubtoage(ibci[c(1, 3, 4, 5, 6)])
 ageden <- inla.tmarginal(hubtoage, imod$marginals.fixed$x)
-plot(ageden, type = "l", xlab = "Age in billions of years",
-    ylab = "density")
+plot(ageden, type = "l", xlab = "Age in billions of years", ylab = "density")
 abline(v = hubtoage(ibci[c(3, 5)]), lty = 2)
 hubtoage(c(10, 15, 20))
-imod <- inla(y ~ x - 1, family = "gaussian",
-    control.fixed = list(mean = 65, prec = 1/(12^2)), data = hubble)
+imod <- inla(y ~ x - 1, family = "gaussian", control.fixed = list(mean = 65, 
+    prec = 1/(12^2)), data = hubble)
 (ibci <- imod$summary.fixed)
 hubtoage(ibci[c(1, 3, 4, 5, 6)])
 (uhub <- hubtoage((2016 + 4004 - 1)/1e+09))
-imod <- inla(y ~ x - 1, family = "gaussian",
-    control.fixed = list(mean = 65, prec = 1/(12^2)), data = hubble)
+imod <- inla(y ~ x - 1, family = "gaussian", control.fixed = list(mean = uhub, 
+    prec = 1/((0.05 * uhub)^2)), data = hubble)
 (ibci <- imod$summary.fixed)
 hubtoage(ibci[c(1, 3, 4, 5, 6)])
 
@@ -45,7 +43,7 @@ hubtoage(ibci[c(1, 3, 4, 5, 6)])
 # Model Selection
 
 
-# Hypothesis testing
+# Hypothesis Testing
 
 
 # Bayesian Computation
